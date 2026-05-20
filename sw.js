50a0699fc5e6ac3c70153e5cb7cf8ac93a0e86ea
MinsYT <70428864+MinsYT@users.noreply.github.com>
2026-05-20 17:56:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "ai-review-pwa-compress-viewer-pen-v5";
+const CACHE_NAME = "ai-review-pwa-version4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
